7b5477c9e108e2ea4e4fced176aa1f612bf14994
bsia0831 <ye08312@naver.com>
2021-11-05 02:29:57 +0900

    main animation

--- js/btnCall.js
+++ js/btnCall.js
@@ -16,3 +16,28 @@ closeBtn.onclick = function(e){
 
     menuMo.classList.remove("on"); 
 }
+
+
+//마우스 호버시 2depth 활성화 
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show(); 
+});
+
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide(); 
+});
+
+//포커스 이동시 2depth 활성화 
+//.each(function(){}) - 배열반복문
+$("#gnb>li").each(function(index){
+
+    //1depth li에서 첫번째 a요소에 focusin이벤트 연결 
+    $("#gnb>li").eq(index).find("a").on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show(); 
+    }); 
+
+    //1depth li에서 마지막 a요소에 focusout 이벤트 연결 
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide(); 
+    })
+})
--- js/scroll.js
+++ js/scroll.js
@@ -5,6 +5,7 @@ const $btns = $("#navi li");
 const $bg = $("#brand");
 let len = $btns.length; //4 
 let baseLine = -10;
+
 /*
 baseLine : 
 해당영역이 브라우저 끝에 닿지 않아도 네비버튼 활성화, 영역 활성화되도록 값을 빼줌   
@@ -49,31 +50,3 @@ $(window).on("scroll", function () {
     }
 
 })
-
-//navi 버튼을 클릭했을 때 
-$("#navi li a").on("click", function (e) {
-    e.preventDefault();
-
-    let target = $(this).attr("href");
-    console.log(target);
-    let targetPos = $(target).offset().top;
-
-    $("html,body").animate({
-        scrollTop: targetPos
-    }, 1000);
-});
-
-/*
-.scrollTop() 선택한 요소의 스크롤바 수직위치값을 반환 
-scrollTop : 스크롤바 위치 
-
-.offset().top 
-: 선택한 요소의 위치를 가져오거나 특정한 위로 이동 
-: 문서끝부터 선택한 요소까지의 거리 
-
-header :0 
-visual : 100
-news : 1000
-brand : 2000 
-crew : 3000
-*/
